569e9d36113fced6c304a5889c955fa7f96c41d7
Mikhail Efremov <jakutenshi@gmail.com>
2023-01-14 23:55:18 +0600

    Add comments to init.el

--- emacs/init.el
+++ emacs/init.el
@@ -1,44 +1,59 @@
+; +----------------------------------------------------------------------------+
+;                НАСТРОЙКИ УДАЛЁННЫХ РЕПОЗИТОРИЕВ ПАКЕТОВ EMACS
+; +----------------------------------------------------------------------------+
+; Определяем репозитории пакетов
 (require 'package)
 (setq package-archives
       '(("GNU ELPA"     . "https://elpa.gnu.org/packages/")
         ("MELPA Stable" . "https://stable.melpa.org/packages/")
         ("MELPA"        . "https://melpa.org/packages/"))
+; Устанавливаем приоритет репозиториев на случай если какие-то недоступны
       package-archive-priorities
       '(("GNU ELPA"     . 10)
         ("MELPA Stable" . 5)
         ("MELPA"        . 0)))
+; Загружаем установленные пакеты
 (package-initialize)
 
-;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;(package-initialize)
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(eval-and-compile
-  (setq use-package-always-ensure t
-        use-package-expand-minimally t))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(global-display-line-numbers-mode t)
- '(inhibit-startup-screen t)
- '(initial-scratch-message "")
- '(package-selected-packages '(apropospriate-theme use-package))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "IBM Plex Mono" :foundry "IBM " :slant normal :weight normal :height 143 :width normal)))))
-
+; +----------------------------------------------------------------------------+
+;                           УСТАНОВКА ПАКЕТОВ EMACS
+; +----------------------------------------------------------------------------+
+; Устанавливаем цветовую тему по-умолчанию
 (use-package apropospriate-theme
   :ensure t
   :config 
   (load-theme 'apropospriate-dark t))
+
+; +----------------------------------------------------------------------------+
+;               НАСТРОЙКА ОБЩИХ ГЛОБАЛЬНЫХ НАСТРОЕК ПО-УМОЛЧАНИЮ
+; +----------------------------------------------------------------------------+
+; Настройки глобальных переменных
+(custom-set-variables
+ ; Включить отображение номеров строк
+ '(global-display-line-numbers-mode t)
+ ; Отключаем приветственный экран Emacs -> при запуске будет открываться черновик
+ '(inhibit-startup-screen t)
+ ; Устанавливаем пустым изначальный текст черновика
+ '(initial-scratch-message "")
+ ; ???
+ '(package-selected-packages '(apropospriate-theme use-package))
+ ; Отключаем полосу прокрутки
+ '(scroll-bar-mode nil)
+ ; Отключаем панель инструментов
+ '(tool-bar-mode nil)
+ '(column-number-mode t))
+
+; Устанавливаем шрифт по-умолчанию
+(custom-set-faces
+ '(default ((t (:family "IBM Plex Mono"
+		:foundry "IBM "
+		:slant normal
+		:weight normal
+		:height 143
+		:width normal)))))
+; +----------------------------------------------------------------------------+
+;                             НАСТРОЙКИ ORG-MODE
+; +----------------------------------------------------------------------------+
+
+
+
